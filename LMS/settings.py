9546a7e635093eea4641bf46ede0315bcd914059
Manwal <manwal@Manwals-MacBook-Pro.local>
2021-11-14 09:04:19 +0530

    Initial commit 2

--- LMS/settings.py
+++ LMS/settings.py
@@ -76,14 +76,27 @@ WSGI_APPLICATION = 'LMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+  'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd68opsh23odobb',
+
+        'USER': 'enqsfbjujapgsk',
+
+        'PASSWORD': 'cd4fda84da1f72d12eabd8387f8f08aa4453540bc42618a59aec499a4635edd4',
+
+        'HOST': 'ec2-3-228-134-188.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+  }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
